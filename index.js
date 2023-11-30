35cb279b205dd488c6cbcdd1832b42c94d2d346b
Igor <147436922+Needforyou@users.noreply.github.com>
2023-11-30 16:53:38 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+let userName = "Krimz"
+
+function quantidadeVitoriasDerrotas(numA, numB){
+        let resultadoCalculo = 75 - 45
+        return resultadoCalculo
+}
+let resultado = quantidadeVitoriasDerrotas(25,10)
+
+if (resultado < 10 ){
+    console.log(userName + " tem de saldo " + resutado + " e está no nível de Ferro")
+}
+else if (resultado > 10 && resultado >= 20){
+    console.log(userName + " tem de saldo " + resultado + " e está no nível de Bronze")
+}
+else if (resultado > 20 && resultado >= 50){
+    console.log(userName + " tem de saldo " + resultado + " e está no nível de Prata")
+}
+else if (resultado > 50 && resultado >= 80){
+    console.log(userName + " tem de saldo " + resultado + " e está no nível de Ouro")
+}
+else if (resultado > 80 && resultado >= 90){
+    console.log(userName + " tem de saldo " + resultado + " e está no nível de Diamante")
+}
+else if (resultado > 90 && resultado >= 100){
+    console.log(userName + " tem de saldo " + resultado + " e está no nível de Lendário")
+}
+else if (resultado >= 100){
+    console.log(userName + " tem de saldo " + resultado + " e está no nível de Imortal")
+}
